aecffc0d674ef20bb7e2ef9a2b4e937f3335b4f3
wellingtonctm <w.luiscm@gmail.com>
2024-10-22 16:06:10 -0300

    refactor: criar serviço de websocket

---
+++ src/services/WebSocketService.tsx
@@ -0,0 +1,40 @@
+class WebSocketService {
+    private socket: WebSocket;
+  
+    constructor(private url: string) {
+      this.socket = new WebSocket(url);
+    }
+  
+    connect(
+      onMessage: (data: any) => void,
+      onError?: (error: Event) => void,
+      onClose?: (event: CloseEvent) => void
+    ): void {
+      this.socket.onmessage = (event: MessageEvent) => {
+        onMessage(JSON.parse(event.data));
+      };
+  
+      this.socket.onerror = (error: Event) => {
+        console.error('WebSocket error:', error);
+        if (onError) onError(error);
+      };
+  
+      this.socket.onclose = (event: CloseEvent) => {
+        console.log('WebSocket closed:', event);
+        if (onClose) onClose(event);
+      };
+    }
+  
+    sendMessage<T>(message: T): void {
+      if (this.socket.readyState === WebSocket.OPEN) {
+        this.socket.send(JSON.stringify(message));
+      }
+    }
+  
+    disconnect(): void {
+      this.socket.close();
+    }
+  }
+  
+  export default WebSocketService;
+  
