09da11a2f683dcbdbd000b5ceda78342bca6ab1a
wellingtonctm <w.luiscm@gmail.com>
2024-10-22 16:01:58 -0300

    feat: organização em rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import Message from './pages/Messages';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Message from '@/pages/Messages';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import FormMessage from '@/pages/form-message';
+import { AuthProvider } from './context/AuthProvider';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Message />} />
+        <Route path='/' element={<AuthProvider><Outlet /></AuthProvider>}>
+          <Route index  element={<FormMessage />} />
+          <Route path='/evento' element={<Message />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   );
