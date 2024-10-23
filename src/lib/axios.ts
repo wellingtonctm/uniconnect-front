2ee1693e76ac2cdce4e519bef3820789a98a752c
wellingtonctm <w.luiscm@gmail.com>
2024-10-23 17:42:16 -0300

    build: modificar url para servidor web

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://192.168.1.100:5103',
+    baseURL: 'https://uniconnect-back.up.railway.app',
     headers: {
         'Content-Type': 'application/json'
     }
